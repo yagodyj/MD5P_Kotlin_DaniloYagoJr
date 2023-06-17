1b91fe6e5839f07e2b10ad34d852c760deb6714f
Dyj Yago <dyj.yago2000@gmail.com>
2023-06-17 18:34:45 +0800

    commit

--- src/main/kotlin/kotlinbasics/KotlinProgram30.kt
+++ src/main/kotlin/kotlinbasics/KotlinProgram30.kt
@@ -1,12 +1,12 @@
 package kotlinbasics
-//
-//import mu.KotlinLogging
-//
-//val logger = KotlinLogging.logger{}
-//fun main(){
-//    logger.trace { "This is trace log" }
-//    logger.debug { "This is debug log" }
-//    logger.info { "This is info log" }
-//    logger.warn { "This is warn log" }
-//    logger.error { "This is error log" }
-//}
+
+import mu.KotlinLogging
+
+val logger = KotlinLogging.logger{}
+fun main(){
+    logger.trace { "This is trace log" }
+    logger.debug { "This is debug log" }
+    logger.info { "This is info log" }
+    logger.warn { "This is warn log" }
+    logger.error { "This is error log" }
+}
